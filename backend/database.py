a299bd8c40852fbc2be886d3746c720ed01f9d82
Thidar Aung <thidaraung00iii@gmail.com>
2026-09-16 19:02:07 +0700

    Prepare JobFlow AI for deployment

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 import os
-
 import psycopg
+
 from dotenv import load_dotenv
 from psycopg.rows import dict_row
 
@@ -9,10 +9,6 @@ load_dotenv()
 
 def get_connection():
     return psycopg.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
+        os.getenv("DATABASE_URL"),
         row_factory=dict_row,
     )
--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,18 @@ from routers.jobs import router as jobs_router
 from routers.matching import router as matching_router
 from routers.candidate import router as candidate_router
 from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
 
+FRONTEND_URL = os.getenv(
+    "FRONTEND_URL",
+    "http://localhost:3000"
+)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
